53028dacd58c276c37afc9bcb617d2f8a0ea6e78
PranavPraveen13 <pranavpraveenpoyil@gmail.com>
2024-03-18 12:52:06 +0100

    Task 2 Completed

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-# Generate a list of squares of numbers from 1 to 10 using list comprehensions
-squares = [x**2 for x in range(1, 11)]
+def generate_squares(start, end):
+    squares = [x**2 for x in range(start, end + 1)]
+    return squares
 
-# Print the list of squares
-print(squares)
+# Get user input for the range
+start = int(input("Enter the start of the range: "))
+end = int(input("Enter the end of the range: "))
 
-#made a new change for a new branch
+# Validate input
+if start > end:
+    print("Error: Start value must be less than or equal to end value.")
+else:
+    squares = generate_squares(start, end)
+    print("List of squares within the range:", squares)
