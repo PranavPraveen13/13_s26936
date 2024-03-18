7f0232c14a1a28073ea0de21275d701ea756e8f3
PranavPraveen13 <pranavpraveenpoyil@gmail.com>
2024-03-18 12:53:45 +0100

    Task 3 Completed

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-def generate_squares(start, end):
-    squares = [x**2 for x in range(start, end + 1)]
-    return squares
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        squares = [x**2 for x in range(start, end + 1)]
+        return squares
 
-# Get user input for the range
+# Example usage:
+generator = SquareGenerator()
 start = int(input("Enter the start of the range: "))
 end = int(input("Enter the end of the range: "))
 
@@ -10,5 +12,5 @@ end = int(input("Enter the end of the range: "))
 if start > end:
     print("Error: Start value must be less than or equal to end value.")
 else:
-    squares = generate_squares(start, end)
+    squares = generator.generate_squares(start, end)
     print("List of squares within the range:", squares)
