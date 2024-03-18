29799aa7dbc908b175c3cf2426dd370e08f8c02a
PranavPraveen13 <pranavpraveenpoyil@gmail.com>
2024-03-18 12:57:16 +0100

    Task 4 Completed

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import math
+
 class SquareGenerator:
     def generate_squares(self, start, end):
+
         squares = [x**2 for x in range(start, end + 1)]
         return squares
+
+    def calculate_square_roots(self, numbers):
+
+        square_roots = [math.sqrt(num) for num in numbers]
+        return square_roots
 
 # Example usage:
 generator = SquareGenerator()
@@ -13,4 +21,6 @@ if start > end:
     print("Error: Start value must be less than or equal to end value.")
 else:
     squares = generator.generate_squares(start, end)
+    square_roots = generator.calculate_square_roots(squares)
     print("List of squares within the range:", squares)
+    print("Square roots of the numbers:", square_roots)
