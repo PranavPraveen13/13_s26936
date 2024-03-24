4bebeac937f62556afa57a3620d2e7207ac1264b
Pranav Praveen <pranavrpaveenpoyil@gmail.com>
2024-03-24 13:33:49 +0100

    Task 10 Completed

--- square_generator/square_generator.py
+++ square_generator/square_generator.py
@@ -1,13 +1,11 @@
+from abc import ABC, abstractmethod
 import math
 
-class SquareGenerator:
+class SquareGenerator(ABC):
+    @abstractmethod
     def generate_squares(self, start, end):
 
-        if end < start:
-            raise ValueError("End of the range cannot be less than the start.")
-
-        squares = [x**2 for x in range(start, end + 1)]
-        return squares
+        pass
 
     def calculate_square_roots(self, numbers):
 
@@ -30,3 +28,14 @@ class CubicGenerator(SquareGenerator):
 
         cubes = [x**3 for x in range(start, end + 1)]
         return cubes
+
+# Example usage:
+generator = CubicGenerator()
+start = int(input("Enter the start of the range: "))
+end = int(input("Enter the end of the range: "))
+
+squares = generator.generate_squares(start, end)
+if squares:
+    square_roots = generator.calculate_square_roots(squares)
+    print("List of squares within the range:", squares)
+    print("Square roots of the numbers:", square_roots)
